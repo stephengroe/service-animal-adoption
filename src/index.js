be534a9c56431cf9b6380b63565e2dc0413ff23a
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-06 22:12:55 -0700

    Fix index bug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import 
+import "./style.css";
+
+function initializePage() {
+  const body = document.querySelector("body");
+  const h1 = document.createElement("h1");
+
+  h1.textContent = "Welcome to PupAbility!";
+
+  body.append(h1);
+}
+
+initializePage();
